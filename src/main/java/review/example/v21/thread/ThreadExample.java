187fbfebec6916f74583a4e0b27e4d3ea8eee8f3
Gina Lin <ginalin@alum.ccu.edu.tw>
2024-03-18 12:30:55 +0800

    add on

--- src/main/java/review/example/v21/thread/ThreadExample.java
+++ src/main/java/review/example/v21/thread/ThreadExample.java
@@ -1,13 +1,16 @@
 package review.example.v21.thread;
 
 public class ThreadExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // Usage - Thread
         for (int i = 0; i < 5; i++) {
-            MyRunnableThread myThread = new MyRunnableThread(i);
-            Thread thread = new Thread(myThread);
-            thread.start(); // add new thread
+            MyRunnableThread myRunnableThread = new MyRunnableThread(i);
+            Thread myThread = new Thread(myRunnableThread);
+            myThread.start(); // add new thread
+
+            myThread.join(); // this executes thread one by one
+//            myThread.isAlive();
         }
 
 //        throw new RuntimeException("The exception won't affect existing threads");
